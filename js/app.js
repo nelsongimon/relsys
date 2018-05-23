ad4ca77f609b396b5dba108ce8341e2d275e645f
Nelson Gimón <nelson.gimon@gmail.com>
2018-05-22 21:29:17 -0400

    Tercera Correcion

--- js/app.js
+++ js/app.js
@@ -84,5 +84,22 @@ $(function(){
 	});
 	wow.init();
 
+    $('.dropdown-toggle').mouseover(function() {
+        $('.dropdown-menu').show();
+    })
+
+    $('.dropdown-toggle').mouseout(function() {
+        t = setTimeout(function() {
+            $('.dropdown-menu').hide();
+        }, 100);
+
+        $('.dropdown-menu').on('mouseenter', function() {
+            $('.dropdown-menu').show();
+            clearTimeout(t);
+        }).on('mouseleave', function() {
+            $('.dropdown-menu').hide();
+        })
+    });
+
 })
 
