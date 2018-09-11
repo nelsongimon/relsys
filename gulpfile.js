99384d4128137bcfe1940f5ad5f38a1e1b5ad2b4
Nelson Gimón <nelson.gimon@gmail.com>
2018-09-11 03:14:02 -0400

    Rensponsive Finalizado version 1.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const minifycss = require('gulp-minify-css')
 
 gulp.task('styles', () => {
 		gulp.src('sass/app.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('css'))
+	})
+
+gulp.task('css', () =>{
+		gulp.src('css/app.css')
+		.pipe(minifycss())
+		.pipe(gulp.dest('css/'))
 	})
 
 gulp.task('default', () => {
